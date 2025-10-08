950704f94a58a4ba000fca41e4c7231ceeff7788
Yan Naing Aung <yannaingaung2209@gmail.com>
2025-10-08 07:21:31 +0630

    node module system

---
+++ module1.js
@@ -0,0 +1,5 @@
+const myName = "Mg Mg";
+const myAge = 21;
+
+module.exports = { myName, myAge };
+module.exports = { myName, myAge };
---
+++ module2.js
@@ -0,0 +1,4 @@
+const { myName, myAge } = require("./module1");
+
+console.log("My name is " + myName + ".");
+console.log("I am " + myAge + " years old.");
