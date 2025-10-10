67fd242dfe36bbbf053a31fb7850a375f9e63933
Yan Naing Aung <yannaingaung2209@gmail.com>
2025-10-10 22:56:07 +0630

    added to :nodejs route system usage

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  let fileName;
+  switch (req.url) {
+    case "/":
+      fileName = "home.html";
+      break;
+    case "/about":
+      fileName = "about.html";
+      break;
+    case "/contact":
+      fileName = "contact.html";
+      break;
+    default:
+      fileName = "404.html";
+      break;
+  }
+
   res.setHeader("Content-Type", "text/html");
-  fs.readFile("./views/home.html", (err, data) => {
+  fs.readFile("./views/" + fileName, (err, data) => {
     if (err) {
       console.log("An error occurred :", err);
       res.end();
