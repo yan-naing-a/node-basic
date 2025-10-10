6bd07ebc299094986411b1456874dc38b5dd1972
Yan Naing Aung <yannaingaung2209@gmail.com>
2025-10-10 22:22:58 +0630

    added to :separate response data as file data

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
-  res.write("<h1>I am testing node js server now</h1>");
-  res.end();
+  fs.readFile("./views/home.html", (err, data) => {
+    if (err) {
+      console.log("An error occurred :", err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
