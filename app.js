2f3557ba10d1600dffa5127e4d948064efab1f3f
Yan Naing Aung <yannaingaung2209@gmail.com>
2025-10-14 17:33:48 +0630

    new :express js start

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+app.get("/contact", (req, res) => {
+  res.sendFile("./views/contact.html", { root: __dirname });
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
