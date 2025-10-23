7df52051b6cdde5b846255d956eac5603c7dfda4
Yan Naing Aung <yannaingaung2209@gmail.com>
2025-10-23 20:43:10 +0630

    added to : create blog from UI(form post)

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Blog = require("./models/Blog");
 const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 const mongoUrl =
   "mongodb+srv://yannaingaung:test1234@cluster0.ftvrb2i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -26,32 +27,26 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "layouts/default");
 
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(express.static("public"));
-
-app.get("/add-blog", async (req, res) => {
-  const blog = new Blog({
-    title: "My Blog 3",
-    introduction: "My Blog intro 3",
-    body: "My Blog body 3",
-  });
-
-  await blog.save();
-  res.send("Blog saved");
-});
 
 app.get("/blogs/create", async (req, res) => {
   res.render("blogs/create", { title: "Blog Create" });
 });
 app.get("/", async (req, res) => {
   const blogs = await Blog.find().sort({ createdAt: -1 });
-
   res.render("home", { blogs, title: "Home" });
 });
 app.get("/single-blog", async (req, res) => {
   const blog = await Blog.findById("68f8d17c9b433efb1a34792a");
-
   res.json(blog);
+});
+
+app.post("/blogs", async (req, res) => {
+  const { title, introduction, body } = req.body;
+  const blog = new Blog({ title, introduction, body });
+  await blog.save();
+  res.redirect("/");
 });
 
 app.get("/about", (req, res) => {
